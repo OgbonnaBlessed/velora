8b610588fcc01c5e9be68ebe7fd85db733cd461e
Ogbonna Blessed <blessedlyrics11@gmail.com>
2024-11-15 10:46:03 +0100

    first commit

---
+++ client/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export const content = [
+  "./src/*/.{js,jsx,ts,tsx}", // Scan all files in the src folder
+];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
